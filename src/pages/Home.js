3ab7f338e2504b63d7469fabf8e0e56050135d80
yoonbumtae <yoonbumtae@gmail.com>
2020-09-16 21:58:01 +0900

    Home 라이브러리 정보 가져오기

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import packageJson from './../../package.json'
 
 const Home = () => {
+
+    const dependencies = packageJson.dependencies
+    const depElements = []
+    for(let key in dependencies) {
+        depElements.push(<li key={key}>{`${key}: ${dependencies[key]}`}</li>)
+    }
+
     return (
         <Container>
             <h3>사용된 라이브러리 및 프레임워크</h3>
             <ul>
-                <li>axios: ^0.20.0</li>
-                <li>react-router-dom: ^5.2.0</li>
-                <li>@material-ui/core: ^4.11.0</li>
+                {depElements}
             </ul>
         </Container>
         );
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,24 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import {Container, Box, Paper, LinearProgress} from '@material-ui/core';
-import axios from "axios"; 
+import { Container, Box, Grid, Paper, LinearProgress } from '@material-ui/core';
+import Rating from '@material-ui/lab/Rating';
+import axios from "axios";
 
 const mapToComponent = data => {
     return data.map((movie, i) => {
         return (
-            <Paper>
-                <Box mb={2} p={2} key={i}>
-                    <p><img src={movie.medium_cover_image} /></p>
-                    <p>Title: {movie.title}</p>
-                    <p>Summary: {movie.summary}</p>
-                    <p>Rating: {movie.rating}</p>
-                    <p>Url: {movie.url}</p>
-                </Box>
-            </Paper>
+            <Box display="flex" justifyContent="center" mb={5} p={2} key={i} width="75%" >
+                <Paper>
+                    <Grid container>
+                        <Grid xs={4}>
+                            <img src={movie.medium_cover_image} alt={movie.title + '의 이미지'}/>
+                        </Grid>
+                        <Grid xs={6}>
+                            <Grid>
+                                <h3>{movie.title}</h3>
+                            </Grid>
+                            <Grid>
+                                <p>{movie.summary}</p>
+                            </Grid>
+                        </Grid>
+                        <Grid xs={2}>
+                            <Grid>
+                                <Box component="fieldset" mb={3} borderColor="transparent">
+                                    <p>평점: {(movie.rating)} / 10</p>
+                                    <Rating name="read-only" value={(movie.rating / 2)} precision={0.05} readOnly />
+                                </Box>
+                            </Grid>
+                            <Grid>
+                                [자세히 보기]
+                        </Grid>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Box>
+
         );
     })
 }
 
-const Movie = ({match}) => {
+const Movie = ({ match }) => {
 
     const [data, setData] = useState(null)
     const [isLoading, setLoading] = useState(true)
@@ -32,24 +53,24 @@ const Movie = ({match}) => {
 
                 setData(result.data);
                 setLoading(false) // 스피너 false
-            } catch(err) {
+            } catch (err) {
 
             }
-     
+
         };
-     
+
         fetchData();
     }, []);
 
     console.log("data", data)
-    
+
     return (
         <Container>
             <h3>영화: {match.params.name}</h3>
             {isLoading === true ? <LinearProgress /> : null}
             {data && mapToComponent(data.data.movies)}
         </Container>
-        );
+    );
 
 }
 
